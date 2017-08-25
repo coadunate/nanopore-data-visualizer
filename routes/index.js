fbc98687795005953a9d539a891cecd8b9d3cbb8
tayabsoomro <isoomro.t@gmail.com>
2017-08-25 16:45:05 -0600

    chagne a couple of things

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
 var express = require('express');
+// var hdf5 = require('hdf5').hdf5;
+// var h5lt = require('hdf5').h5lt;
 var router = express.Router();
+
+// var Access = require('hdf5/lib/globals').Access;
+//
+// var directory = __dirname + '/../includes/data/fast5/' // directory where fast5
+// // .. files are stored.
+// var filename = 'file1.fast5'
+//
+//
+// var file = new hdf5.File(directory + filename, Access.ACC_RDONLY);
+//
+// var group = file.openGroup('/Analyses/Basecall_1D_001/BaseCalled_template');
+//
+// var dataset = h5lt.readDataset(group.id,'Events');
+// //
+// // console.log(JSON.stringify(dataset));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
